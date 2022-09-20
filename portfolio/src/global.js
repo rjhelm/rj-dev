ab47307f7d54c468ee28ff97b5555a9b3461bf39
rjhelm <rjhelm@github.io>
2022-09-19 19:34:39 -0600

    created initial app file structure and dependencies

---
+++ portfolio/src/global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::after,
+    *::before {
+        box-sizing: border-box;
+    }
+
+    body {
+        align-items: center;
+        background: ${({ theme}) => theme.body};
+        color: ${({ theme }) => theme.text};
+        display: flex;
+        // flex-direction
+        // justify-content
+        // height
+        // margin
+        // padding
+        font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+        transition: all 0.25s linear;
+    }
+`;
---
+++ portfolio/src/theme.js
@@ -0,0 +1,9 @@
+const lightTheme = {
+    name: 'light'
+}
+
+const darkTheme = {
+    name: 'dark'
+}
+
+export const themes = { light: lightTheme, dark: darkTheme };
