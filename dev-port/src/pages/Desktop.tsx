db6dc4b251c829ba03759ca46c4ad455339e575c
rjhelm <rjhelm@github.io>
2022-10-17 17:50:11 -0600

    created page view initial functions and a few helpers || configured eslint for type

---
+++ dev-port/src/pages/Desktop.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import type { RefObject } from 'react';
+
+// Components
+
+interface DesktopState {
+
+}
+
+export default function Desktop(props: MacActions) {
+    const [state, setState] = useState({
+
+    }, as DesktopState);
+
+    
+}
---
+++ dev-port/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useAppSelector } from "~/redux/hooks";
+import { wallpapers, user } from "~/configs";
+import type { MacActions } from "~/types";
+
+export default function Login(props: MacActions) {
+    
+}
