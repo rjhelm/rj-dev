9787e5a7318b5df7960d6c46208dcc5569785fe5
rjhelm <rjhelm@github.io>
2022-10-17 22:18:15 -0600

    added hooks and configured store for use in components working on

---
+++ dev-port/src/components/Window.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { Rnd } from 'react-rnd';
+import { useWindowSize } from '~/hooks';
+import { useAppSelector } from '~/redux/hooks';
+
+const FullIcon = ({ size }: { size: number }) => {
+    return (
+
+    )
+};
+
+const ExitFullIcon = ({ size }: { size: number }) => {
+    return (
+
+    )
+}
+
+
+const Window = (props: WindowProps) => {
+    
+}
